aee42b38dd1cebd11644b19796fcdff8fdfc6b89
Daniel Persson <mailto.woden@gmail.com>
2017-07-30 21:52:50 +0200

    Initial commit.

---
+++ src/main/java/VersioningDemo.java
@@ -0,0 +1,8 @@
+/**
+ * Versioning demo created by Daniel Persson
+ */
+public class VersioningDemo {
+    public static void main(String args[]) {
+        System.out.println(VersioningDemo.class.getPackage().getImplementationVersion());
+    }
+}
